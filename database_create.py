a6006e75f74241e842be3b16f82e12c214ed5e77
Luiz Felipe Vieira Cordeiro <luizfelipevcordeiro@hotmail.com>
2025-03-16 17:32:23 -0300

    database exemple itens loaded

--- database_create.py
+++ database_create.py
@@ -1,10 +1,10 @@
 import sqlite3 
 
-connection = sqlite3.connect('banco.db')
+connection = sqlite3.connect('./instance/banco_app.db')
 cursor = connection.cursor()
 
 criar_tabela = "CREATE TABLE IF NOT EXISTS hoteis (hotel_id text PRIMARY KEY, nome text, estrelas real, diaria real, cidade text)"
-criar_hotel = "INSERT INTO hoteis VALUES ('alpha2', 'Alpha Hotel', 4.3, 345.30, 'Rio de Janeiro')"
+criar_hotel = "INSERT INTO hoteis VALUES ('alpha3', 'Charlie Hotel', 4.3, 345.30, 'Rio de Janeiro',1)"
 
 cursor.execute(criar_tabela)
 cursor.execute(criar_hotel)
